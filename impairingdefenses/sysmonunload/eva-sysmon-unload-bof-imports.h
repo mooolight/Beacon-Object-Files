79170baf2aa8e579c319ac89fc5bb1cc75f4a00a
mooolight <44820493+mooolight@users.noreply.github.com>
2026-05-30 20:02:29 -0600

    Add files via upload

---
+++ impairingdefenses/sysmonunload/eva-sysmon-unload-bof-imports.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef BOF
+	// Kernel32
+	WINBASEAPI 		DWORD 	WINAPI KERNEL32$GetLastError(VOID);
+	#define GetLastError KERNEL32$GetLastError
+	DECLSPEC_IMPORT HANDLE  WINAPI KERNEL32$GetCurrentProcess();
+	#define GetCurrentProcess KERNEL32$GetCurrentProcess
+
+
+	// Advapi32
+	WINADVAPI BOOL WINAPI ADVAPI32$OpenProcessToken(HANDLE,DWORD,PHANDLE);
+	#define OpenProcessToken ADVAPI32$OpenProcessToken
+	WINADVAPI BOOL WINAPI ADVAPI32$LookupPrivilegeValueA(LPCSTR ,LPCSTR ,PLUID);
+	#define LookupPrivilegeValueA ADVAPI32$LookupPrivilegeValueA
+	WINADVAPI BOOL WINAPI ADVAPI32$AdjustTokenPrivileges(HANDLE,BOOL,PTOKEN_PRIVILEGES,DWORD,PTOKEN_PRIVILEGES,PDWORD);
+	#define AdjustTokenPrivileges ADVAPI32$AdjustTokenPrivileges
+#endif
