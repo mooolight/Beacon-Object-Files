0ea269b923526dc577d6c310488da725f1734a81
mooolight <44820493+mooolight@users.noreply.github.com>
2026-06-02 16:20:59 -0600

    Update procenum-classic-bof.c

--- collection/procenum/procenum-classic-bof.c
+++ collection/procenum/procenum-classic-bof.c
@@ -5,21 +5,7 @@
    - Heavily dependent on code by reenz0h (twitter: @SEKTOR7net) => Red Team Operator course code template Classic Process enumeration
 
 */
-#include <windows.h>
-#include <stdio.h>
-#include <tlhelp32.h>
-#include "..\beacon.h"
-#include "..\bofdefs.h"
-// 
-// Kernel32
-DECLSPEC_IMPORT HANDLE WINAPI KERNEL32$CreateToolhelp32Snapshot(DWORD ,DWORD);  // Nt equivalent == 
-DECLSPEC_IMPORT BOOL   WINAPI KERNEL32$Process32First(HANDLE, LPPROCESSENTRY32);  // Nt equivalent == 
-DECLSPEC_IMPORT BOOL   WINAPI KERNEL32$CloseHandle(HANDLE);  // Nt equivalent == 
-DECLSPEC_IMPORT BOOL   WINAPI KERNEL32$Process32Next(HANDLE, LPPROCESSENTRY32);  // Nt equivalent == 
-
-// Msvcrt
-DECLSPEC_IMPORT int __cdecl MSVCRT$lstrcmpiA(LPCSTR ,LPCSTR);
-
+#include "procenum-classic.h"
 
 int FindTarget(const char *procname) {
 	HANDLE hProcSnap;
@@ -27,29 +13,30 @@ int FindTarget(const char *procname) {
 	int pid = 0;
 	
 	// create a snapshot
-	hProcSnap = KERNEL32$CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	hProcSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	if (INVALID_HANDLE_VALUE == hProcSnap) return 0;
 			
 	pe32.dwSize = sizeof(PROCESSENTRY32); 
 	
 	// and start to parse it from the first entry
-	if (!KERNEL32$Process32First(hProcSnap, &pe32)) {
-		KERNEL32$CloseHandle(hProcSnap);
-		BeaconPrintf(CALLBACK_OUTPUT, "Failed to get the next process in the linked list.\n");
+	if (!Process32First(hProcSnap, &pe32)) {
+		CloseHandle(hProcSnap);
+		PRINT("Failed to get the next process in the linked list.\n");
 		return -1;
 	}
 	
-	while (KERNEL32$Process32Next(hProcSnap, &pe32)) {
-		if (MSVCRT$lstrcmpiA(procname, pe32.szExeFile) == 0) {
+	while (Process32Next(hProcSnap, &pe32)) {
+		if (lstrcmpiA(procname, pe32.szExeFile) == 0) {
 				pid = pe32.th32ProcessID;
-				BeaconPrintf(CALLBACK_OUTPUT, "Found the target process.\n");
+				PRINT("Found the target process.\n");
 				break;
 		}
 	}
-	KERNEL32$CloseHandle(hProcSnap);
+	CloseHandle(hProcSnap);
 	return pid;
 }
 
+#ifdef BOF
 
 // Usage: procenum-classic <procName>
 void go(char *args, int alen) {
@@ -64,6 +51,26 @@ void go(char *args, int alen) {
 	
 	//int pid = FindTarget("notepad.exe");
 	int pid = FindTarget(procName);
-	BeaconPrintf(CALLBACK_OUTPUT, "Process:%s - %s%d)\n", procName ,pid > 0 ? "found at PID: (" : "NOT FOUND (", pid);
+	PRINT("Process:%s - %s%d)\n", procName ,pid > 0 ? "found at PID: (" : "NOT FOUND (", pid);
 	return;
 }
+
+#else
+
+int main(){
+	char* procName = "calculator.exe";
+	/*
+	// Prepare data parser
+	datap parser;
+	BeaconDataParse(&parser, args, alen);
+	
+	// Unpack Data
+	procName = BeaconDataExtract(&parser, NULL);
+	*/
+	//int pid = FindTarget("notepad.exe");
+	int pid = FindTarget(procName);
+	PRINT("Process:%s - %s%d)\n", procName ,pid > 0 ? "found at PID: (" : "NOT FOUND (", pid);
+	return 0;
+}
+
+#endif
