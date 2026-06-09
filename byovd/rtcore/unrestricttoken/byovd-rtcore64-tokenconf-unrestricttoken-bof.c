c1f2871b6c54a66742658dc22a1282603574116f
mooolight <44820493+mooolight@users.noreply.github.com>
2026-06-09 15:44:46 -0600

    Update byovd-rtcore64-tokenconf-unrestricttoken-bof.c

--- byovd/rtcore/unrestricttoken/byovd-rtcore64-tokenconf-unrestricttoken-bof.c
+++ byovd/rtcore/unrestricttoken/byovd-rtcore64-tokenconf-unrestricttoken-bof.c
@@ -1071,6 +1071,7 @@ BOOL GetSystemEproc(DWORD64 * obj) {
     if (status != 0) {
         PRINT("[!] NtQuerySystemInformation failed!\n");
         result = FALSE;
+		free(handleInfo);
 		return result;
     }
 	
@@ -1083,7 +1084,8 @@ BOOL GetSystemEproc(DWORD64 * obj) {
 		result = TRUE;
 	} else
 		result = FALSE;
-	
+
+	free(handleInfo);
 	PRINT("[+] Successfully extracted the SYSTEM process EPROC address!\n");
 	
 	return result;
